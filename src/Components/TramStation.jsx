adfa090025e9d715271616c782768b8f6d9b6abc
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-23 22:36:00 +0200

    stations display the next tram

--- src/Components/TramStation.jsx
+++ src/Components/TramStation.jsx
@@ -1,22 +1,46 @@
+import { useState,useEffect } from "react";
 import { Popup, Marker } from "react-leaflet";
 import getStationWaitTime from "../scripts/getStationWaitTime";
+import axios from "axios";
+
+function TramStation({ station }) {
+  const [nextTrams, setNextTrams] = useState(null);
 
 
-function TramStation({station}) {
-    console.log(station)
+  function getNextTrams(id) {
+    axios
+      .get(`https://open.tan.fr/ewp/tempsattente.json/${id}`)
+      .then((response) => {
+        setNextTrams(response.data);
+      });
+  }
+
+  useEffect(()=>{
+    getNextTrams(station.codeLieu)
+  },[])
+  
+
+
+
+  if (nextTrams) console.log(nextTrams[0]);
+
+
   return (
     <Marker position={[station.position.lat, station.position.lon]}>
       <Popup>
         {station.libelle}
         <button
           type="button"
-          onClick={() => console.log(getStationWaitTime(station.codeLieu))}
+          onClick={() => getNextTrams(station.codeLieu)}
         >
           refresh
         </button>
+        <p>
+          next tram : {nextTrams ? nextTrams[0].terminus+" in "+nextTrams[0].temps : "no info to display"}
+        </p>
       </Popup>
     </Marker>
   );
 }
 
-export default TramStation
+export default TramStation;
