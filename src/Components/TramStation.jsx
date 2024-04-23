6de83a59c7af9d1647fb238d86bc178cfd4cb9aa
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-04-23 23:22:01 +0200

    Changed the icons

--- src/Components/TramStation.jsx
+++ src/Components/TramStation.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import { Popup, Marker } from "react-leaflet";
 import getNextTrams from "../scripts/getNextTram";
+import L from 'leaflet';
+import line1Icon from '../assets/icons/Line1_Station.png'
+
+
+const iconLine1 = L.icon({
+  iconUrl: line1Icon,
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [0, -16],
+});
 
 function TramStation({ station }) {
   const [nextTrams, setNextTrams] = useState(null);
@@ -12,15 +22,10 @@ function TramStation({ station }) {
   if (nextTrams) console.log(nextTrams);
 
   return (
-    <Marker position={[station.position.lat, station.position.lon]}>
+    <Marker position={[station.position.lat, station.position.lon]} icon={iconLine1}>
       <Popup>
-        {station.libelle}
-        <button
-          type="button"
-          onClick={() => getNextTrams(station.codeLieu, setNextTrams)}
-        >
-          refresh
-        </button>
+        <h3>{station.libelle}</h3>
+       
         {nextTrams ? (
           <ul>
             {nextTrams.map(tram => {
@@ -32,6 +37,12 @@ function TramStation({ station }) {
         ) : (
           <p>no info to display, press refresh</p>
         )}
+         <button
+          type="button"
+          onClick={() => getNextTrams(station.codeLieu, setNextTrams)}
+        >
+          refresh
+        </button>
       </Popup>
     </Marker>
   );
